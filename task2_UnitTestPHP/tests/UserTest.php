8d501515493c05e7fc08d4b8e3f7be21b157f7c2
20vlad052 <mr.ralenskiy@mail.ru>
2024-12-07 12:26:51 +0300

    Убрал лишние пробелы и ненужную футкнцию

--- task2_UnitTestPHP/tests/UserTest.php
+++ task2_UnitTestPHP/tests/UserTest.php
@@ -4,11 +4,6 @@ namespace App;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase {
-    protected function setUp(): void {
-        // Сбрасываем массив пользователей перед каждым тестом
-        UserCreate::setUsers([]); // Предполагая, что у вас есть метод setUsers()
-    }
-
     public function testCreateUser() {
         $user = UserCreate::create('Vlad');
         $this->assertNotNull($user);
@@ -24,40 +19,24 @@ class UserTest extends TestCase {
         $this->assertEquals('Vlad', $foundUser->getName());
         $notFoundUser = UserCreate::findByName('Omar');
         $this->assertNull($notFoundUser);
-
     }
     public function testCreateAndDeleteUser() {
-     
         $user = UserCreate::create('Vlad');
         $this->assertNotNull($user);
         $this->assertEquals('Vlad', $user->getName());
-
-    
         $users = UserCreate::getUsers();
         $this->assertCount(1, $users);
         $this->assertSame($user, $users[0]);
-
-    
         UserDelete::delete($user);
-
-      
         $users = UserCreate::getUsers();
         $this->assertCount(0, $users);
     }
     public function testUpdateUserName() {
-       
         $user = UserCreate::create('John Doe');
-
-      
         $user->setName('Vlad');
-
-       
         $this->assertEquals('Vlad', $user->getName());
-
-        
         $foundUser = UserCreate::findByName('Vlad');
         $this->assertSame($user, $foundUser);
-
         $notFoundUser = UserCreate::findByName('John Doe');
         $this->assertNull($notFoundUser);
     }
